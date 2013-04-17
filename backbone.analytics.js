4a39d2634f9699a7df0dd9ba2295915deb901aeb
Wills Bithrey <wills@potatolondon.com>
2013-04-17 15:29:51 +0100

    Alter tracking logic so it's agnostic to how developers call their routing functions
    Ideally we shouldn't care if the user calls routing functions directly or calls router.navigate({ trigger: true });

--- backbone.analytics.js
+++ backbone.analytics.js
@@ -4,16 +4,45 @@
 // Version 0.0.2
 
 (function() {
-  var _loadUrl = Backbone.History.prototype.loadUrl;
+  var _loadUrl = Backbone.History.prototype.loadUrl
+      _navigate = Backbone.History.prototype.navigate;   
 
   Backbone.History.prototype.loadUrl = function(fragmentOverride) {
-    var matched = _loadUrl.apply(this, arguments),
-        fragment = this.fragment = this.getFragment(fragmentOverride);
-
-    if (!/^\//.test(fragment)) fragment = '/' + fragment;
-    if (window._gaq !== undefined) window._gaq.push(['_trackPageview', fragment]);
-
+    var matched = _loadUrl.apply(this, arguments);
+    Backbone.History.prototype.trackUrl.apply(this);
+    
     return matched;
   };
 
+  Backbone.History.prototype.trackUrl = function () {
+    var fragment = this.fragment;
+
+    // Normalise fragment with leading slash
+    if (!/^\//.test(fragment)) fragment = '/' + fragment;
+
+    // Ensure we aren't double tracking an already tracked fragment
+    if (fragment !== this.lastTrackedFragment) {
+      if (window._gaq !== undefined) window._gaq.push(['_trackPageview', fragment]);
+      this.lastTrackedFragment = fragment;
+    }
+  };
+
+  Backbone.History.prototype.navigate = function(fragment, options) {
+    var result = _navigate.apply(this, arguments);
+
+    if (!options || options === true) options = {
+      track: true,
+      trigger: options
+    };
+    
+    if (_.isUndefined(options.track)) {
+      options.track = true;
+    }
+
+    if (options.track) {
+      this.trackUrl();
+    }
+
+    return result;
+  };
 }).call(this);
